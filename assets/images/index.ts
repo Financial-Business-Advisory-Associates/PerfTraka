dffe612d1aeef98f92581063c6afc7a7b88b29ff
Financial-Business-Advisory-Associates <olabimtansimon@yahoo.com>
2024-09-17 21:03:32 +0100

    Add files via upload

---
+++ assets/images/index.ts
@@ -0,0 +1,14 @@
+import logoSquare from './logo.png';
+import logoRect from './headerLogo.png';
+import defaultUser from './user.png';
+import securityCard from './security-card.png';
+import successCheck from './success.png';
+import logoIcon from './icon.png';
+export {
+  logoSquare,
+  logoRect,
+  logoIcon,
+  defaultUser,
+  securityCard,
+  successCheck,
+};
